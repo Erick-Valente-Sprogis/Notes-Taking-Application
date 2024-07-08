d1d4b3784bef3f00076ddcfa15841fb613f7cb21
Erick Valente Sprogis <103703957+Erick-Valente-Sprogis@users.noreply.github.com>
2024-07-08 13:57:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function addNote() {
     save.addEventListener("click",saveNotes);
     textarea.addEventListener("input", saveNotes);
     trash.addEventListener("click", ()=>{
-        
+
         note.remove();
         saveNotes();
     });
@@ -33,5 +33,24 @@ function saveNotes() {
         localStorage.removeItem("notes");
     } else {
         localStorage.setItem("notes",JSON.stringify(data));
+    };
+};
+
+function loadNotes() {
+    const lsNotes = JSON.parse(localStorage.getItem("notes"));
+
+    if(lsNotes !==null) {
+        lsNotes.forEach(noteText =>{
+
+            addNote();
+
+            const notes = document.querySelectorAll(".note textarea");
+            const lastNote = notes[notes.length -1];
+            lastNote.value = noteText;
+        });
+    }else {
+        addNote();
     }
-}
+};
+
+loadNotes();
